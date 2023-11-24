137054f596e75deccabded84ac740be87e1b4fd9
Nikhil Singh <69163652+Nikhil9971@users.noreply.github.com>
2023-11-24 23:13:10 +0530

    Add files via upload

---
+++ components/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <p>ECommerce 2023</p>
+      <p className="icons">
+        <AiFillInstagram />
+        <AiOutlineTwitter />
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
